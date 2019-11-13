df50ed6d05b4d8d19fc3a1d1ff6544b7022313bb
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-11-13 09:46:28 +0100

    Finished basic befunge implementation

--- defunge.py
+++ defunge.py
@@ -55,10 +55,28 @@ class Cursor:
     def __str__(self):
         return f"C[{self.x} {self.y}]"
 
+    def horizontal_if(self, stack):
+        a = stack.pop()
+        if a == 0:
+            self.change_dir(Direction.RIGHT)
+        else:
+            self.change_dir(Direction.LEFT)
+
+    def vertical_if(self, stack):
+        a = stack.pop()
+        if a == 0:
+            self.change_dir(Direction.DOWN)
+        else:
+            self.change_dir(Direction.UP)
+
+    def skip(self):
+        self.step()
+
 
 class Stack:
     def __init__(self):
         self.entries = []
+        self.out = ""
 
     def push(self, entry):
         self.entries.append(entry)
@@ -70,16 +88,22 @@ class Stack:
         self.push(self.pop() + self.pop())
 
     def subtract(self):
-        self.push(self.pop() - self.pop())
+        a = self.pop()
+        b = self.pop()
+        self.push(b-a)
 
     def multiply(self):
         self.push(self.pop() * self.pop())
 
     def divide(self):
-        self.push(self.pop() / self.pop())
+        a = self.pop()
+        b = self.pop()
+        self.push(int(b/a))
 
     def modulo(self):
-        self.push(self.pop() % self.pop())
+        a = self.pop()
+        b = self.pop()
+        self.push(b % a)
 
     def logic_not(self):
         if self.pop() == 0:
@@ -93,8 +117,40 @@ class Stack:
         else:
             self.push(0)
 
+    def duplicate(self):
+        a = self.pop()
+        self.push(a)
+        self.push(a)
 
+    def swap(self):
+        a = self.pop()
+        b = self.pop()
+        self.push(a)
+        self.push(b)
 
+    def print_num(self):
+        a = self.pop()
+        self.out += a
+
+    def print_ascii(self):
+        a = self.pop()
+        self.out += chr(a)
+
+    def pop_output(self) -> str:
+        out = self.out
+        self.out = ""
+        return out
+
+    def read_num(self):
+        num = int(input("> "))
+        self.push(num)
+
+    def read_ascii(self):
+        char = ord(input("> "))
+        self.push(char)
+
+    def __str__(self):
+        return str(self.entries)
 
 
 def read_file(path: str):
@@ -108,8 +164,6 @@ def read_file(path: str):
         return ret
 
 
-
-
 class Field:
     def __init__(self, field):
         self.field = field
@@ -118,6 +172,11 @@ class Field:
         self.max_x = max(map(len, self.field))-1
 
         self.cursor = None
+
+        for line in self.field:
+            l = len(line)
+            for i in range(self.max_x - l + 1):
+                line.append(' ')
 
     def at(self, x, y):
         return self.field[y][x]
@@ -130,7 +189,19 @@ class Field:
                 else:
                     print(symbol, end='', flush=False)
             print(flush=False)
-        print()
+        print("==========================")
+
+    def get_to_stack(self, stack):
+        x = stack.pop()
+        y = stack.pop()
+        v = self.at(x, y)
+        stack.push(v)
+
+    def put_from_stack(self, stack):
+        x = stack.pop()
+        y = stack.pop()
+        v = stack.pop()
+        self.field[y][x] = chr(v)
 
 
 def change_dir(dir):
@@ -139,12 +210,40 @@ def change_dir(dir):
     return func
 
 
-handlers = {
+cursor_handlers = {
     '>': change_dir(Direction.RIGHT),
     '<': change_dir(Direction.LEFT),
     '^': change_dir(Direction.UP),
     'v': change_dir(Direction.DOWN),
-    '?': Cursor.random_dir
+    '?': Cursor.random_dir,
+    '#': Cursor.skip
+}
+
+stack_handlers = {
+    '*': Stack.multiply,
+    '/': Stack.divide,
+    '+': Stack.add,
+    '-': Stack.subtract,
+    '%': Stack.modulo,
+    '`': Stack.bigger_than,
+    '!': Stack.logic_not,
+    ':': Stack.duplicate,
+    '\\': Stack.swap,
+    '$': Stack.pop,
+    '.': Stack.print_num,
+    ',': Stack.print_ascii,
+    '&': Stack.read_num,
+    '~': Stack.read_ascii
+}
+
+conditional_handlers = {
+    '_': Cursor.horizontal_if,
+    '|': Cursor.vertical_if
+}
+
+field_handler = {
+    'g': Field.get_to_stack,
+    'p': Field.put_from_stack,
 }
 
 
@@ -153,16 +252,42 @@ def parse(path: str):
 
     cursor = Cursor(field)
 
-    while True:
-        cursor.step()
+    stack = Stack()
 
-        if cursor.symbol() in handlers:
-            handlers[cursor.symbol()](cursor)
+    string_mode = False
 
-        print(cursor)
-        print(cursor.symbol())
+    output = ""
+    running = True
+
+    while running:
+        if string_mode:
+            if cursor.symbol() in ['"']:
+                string_mode = False
+            else:
+                stack.push(ord(cursor.symbol()))
+        else:
+            if cursor.symbol() in cursor_handlers:
+                cursor_handlers[cursor.symbol()](cursor)
+            if cursor.symbol() in stack_handlers:
+                stack_handlers[cursor.symbol()](stack)
+            if cursor.symbol().isdigit():
+                stack.push(int(cursor.symbol()))
+            if cursor.symbol() in conditional_handlers:
+                conditional_handlers[cursor.symbol()](cursor, stack)
+            if cursor.symbol() in field_handler:
+                field_handler[cursor.symbol()](field, stack)
+            if cursor.symbol() in ['"']:
+                string_mode = True
+            if cursor.symbol() in ['@']:
+                running = False
+
+        print(stack)
+        output += stack.pop_output()
         field.pretty_print()
-        time.sleep(0.1)
+        print(output)
+
+        time.sleep(0.01)
+        cursor.step()
 
 
 if __name__ == '__main__':
